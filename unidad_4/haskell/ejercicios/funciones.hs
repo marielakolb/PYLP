c6b74b484dce81a4a916671a498484ac66749cf3
SuichiM <MartiniRoqueEsteban@gmail.com>
2020-11-10 10:25:57 -0300

    added haskell exercises

---
+++ unidad_4/haskell/ejercicios/funciones.hs
@@ -0,0 +1,14 @@
+module Funciones where
+
+{- Funciones iniciales -}
+
+-- 1. sucesor
+sucesor :: Integer -> Integer
+sucesor x = x + 1
+
+pre :: Integer -> Integer
+pre x = x - 1 
+
+-- 2. doble 
+doble :: Integer -> Integer
+doble x = x+x
